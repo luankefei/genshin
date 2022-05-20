5058b00ace2af0c2f56dce39d8178d18071b8f28
luankefei <luankefei@gmail.com>
2022-05-20 10:38:14 +0800

    perf(nav): useMemo -> userinfo

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,37 +1,44 @@
+import { useMemo } from "react";
 import ActiveLink from "../ActiveLink/ActiveLink";
 import { Header, Container, User } from "./nav.style";
 
-const Nav = () => (
-  <Header>
-    <div className="logo">原神练度工具</div>
-    <Container>
-      <ul>
-        <li>
-          <ActiveLink activeClassName="active" href="/">
-            <span>角色</span>
-          </ActiveLink>
-        </li>
-        <li>
-          <ActiveLink activeClassName="active" href="/artifact">
-            <span>圣遗物</span>
-          </ActiveLink>
-        </li>
-        <li>
-          <ActiveLink activeClassName="active" href="/me">
-            <span>我的</span>
-          </ActiveLink>
-        </li>
-      </ul>
-    </Container>
-    <User>
-      <span>马拉非</span>
-      <img
-        className="avatar"
-        src="https://imgxiaolai.laiye.com/uQ7jAnAO4nMgjOSU6rtM3ZAPrR3DENNM9R7iaUicGO6js7PUfGH145pf23Twswqh370RJTpam9uBvuUC4blCpjaw.jpeg"
-        alt="avatar"
-      />
-    </User>
-  </Header>
-);
+const Nav = () => {
+  const userInfo = useMemo(() => {
+    return {
+      nickname: "马拉非",
+      avatar:
+        "https://imgxiaolai.laiye.com/uQ7jAnAO4nMgjOSU6rtM3ZAPrR3DENNM9R7iaUicGO6js7PUfGH145pf23Twswqh370RJTpam9uBvuUC4blCpjaw.jpeg",
+    };
+  }, []);
+
+  return (
+    <Header>
+      <div className="logo">原神练度工具</div>
+      <Container>
+        <ul>
+          <li>
+            <ActiveLink activeClassName="active" href="/">
+              <span>角色</span>
+            </ActiveLink>
+          </li>
+          <li>
+            <ActiveLink activeClassName="active" href="/artifact">
+              <span>圣遗物</span>
+            </ActiveLink>
+          </li>
+          <li>
+            <ActiveLink activeClassName="active" href="/me">
+              <span>我的</span>
+            </ActiveLink>
+          </li>
+        </ul>
+      </Container>
+      <User>
+        <span>{userInfo.nickname}</span>
+        <img className="avatar" src={userInfo.avatar} alt="avatar" />
+      </User>
+    </Header>
+  );
+};
 
 export default Nav;
